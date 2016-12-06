f7d7d6068920b6b76da61d4f613d03cb3ff48497
Khasim Peera Durgam <Khasim Peera Durgam>
2016-12-06 08:48:29 +0530

    first commit

---
+++ src/testcases/AppTest.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	@Test
+	public void SampleTest()
+	{
+		System.out.println("ant ant ant ant ant ant ant");
+	}
+
+}
